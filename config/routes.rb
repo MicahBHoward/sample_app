ff75dbcda95096e66e701e11138b2527f1bb1e3b
Micah Howard <micahbhoward@me.com>
2018-03-29 16:29:30 -0400

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -9,4 +7,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
+
+  get '/signup', to: 'users#new'
 end
